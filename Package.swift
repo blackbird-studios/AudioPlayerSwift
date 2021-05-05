072c8732429dec5f757222a7f5b012e8c83d2950
Peter Stuart <peter@peterstuart.org>
2021-05-05 08:09:12 -0400

    Add metadata to Package.swift.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-  name: "AudioPlayer"
+    name: "AudioPlayer",
+    platforms: [.iOS(.v12)],
+    products: [.library(name: "AudioPlayer", targets: ["AudioPlayer"])],
+    targets: [.target(name: "AudioPlayer", path: "Sources")],  
+    swiftLanguageVersions: [.v5]
 )
